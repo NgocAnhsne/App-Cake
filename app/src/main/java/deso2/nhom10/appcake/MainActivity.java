7ed5108705572bac9565845888c15d87200a9892
Quang-Thanh <quangthanhvu711@gmail.com>
2022-12-08 18:41:16 +0700

    menuthanh

--- app/src/main/java/deso2/nhom10/appcake/MainActivity.java
+++ app/src/main/java/deso2/nhom10/appcake/MainActivity.java
@@ -2,13 +2,20 @@ package deso2.nhom10.appcake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
